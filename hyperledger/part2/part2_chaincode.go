be80dbae4bbd63a9f2d596f5394c38a509852f4a
CormierG <CormierG@aetna.com>
2016-09-14 17:33:39 -0400

    first commit and push test

--- hyperledger/part2/part2_chaincode.go
+++ hyperledger/part2/part2_chaincode.go
@@ -16,7 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-
+// Added this line - Gerry Cormier - to see if I am using Git push correctly
 package main
 
 import (
